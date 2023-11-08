eb99aa111e3d9c9133f4d07943b3e1933e221093
Anthony Tagorda <anthtago@gmail.com>
2023-11-08 20:22:36 +0800

    Fixed Navigation Bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-const categorySelector = document.getElementById('category');
+const categorySelector = document.querySelector('.tabs-box');
 const placesList = document.getElementById('places-list');
 const apiKey = '5aa4bbc3a35d4f24a9539fd819bfcf50';
 
-const tabsBox = document.querySelector(".tabs-box"),
-allTabs = tabsBox.querySelectorAll(".tab"),
-arrowIcons = document.querySelectorAll(".icon i");
+const tabsBox = document.querySelector(".tabs-box");
+const allTabs = tabsBox.querySelectorAll(".tab");
+const arrowIcons = document.querySelectorAll(".icon i");
 
 let isDragging = false;
 
@@ -13,6 +13,61 @@ const handleIcons = (scrollVal) => {
     arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
     arrowIcons[1].parentElement.style.display = maxScrollableWidth - scrollVal <= 1 ? "none" : "flex";
 }
+
+// Function to fetch data from specific category
+categorySelector.addEventListener('click', (event) => {
+    if (event.target.classList.contains('tab')) {
+        // Get selected category
+        const selectedCategory = event.target.id;
+
+        // Construct API URL based on the selected category
+        const apiUrl = `https://api.geoapify.com/v2/places?categories=${selectedCategory}&filter=place:51482915cd16265e4059e735029a78663040f00101f90159cdd40000000000c0020992030642616775696f&limit=20&apiKey=${apiKey}`;
+
+        // Fetch data from the API
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                // Clear the places list
+                placesList.innerHTML = '';
+
+                // Iterate through the fetched data and display it
+                data.features.forEach((place) => {
+                    const properties = place.properties;
+                    const raw = properties.datasource.raw;
+
+                    placesList.innerHTML += `
+                        <div class="place">
+                            <h3>${properties.name || 'N/A'}</h3>
+                            <p>Address: ${properties.formatted || 'N/A'}</p>
+                            <p>Email: ${raw['contact:email'] || 'N/A'}</p>
+                            <p>Phone: ${raw['contact:phone'] || 'N/A'}</p>
+                            <p>Mobile: ${raw['contact:mobile'] || 'N/A'}</p>
+                            <p>Website: ${raw['contact:website'] || 'N/A'}</p>
+                            <p>Facebook: ${raw['contact:facebook'] || 'N/A'}</p>
+                            <p>Opening Hours: ${raw.opening_hours || 'N/A'}</p>
+                            <p>Charge: ${raw.charge || 'N/A'}</p>
+                        </div>
+                    `;
+                });
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }
+});
+
+allTabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        // Check if the clicked tab has the "active" class
+        if (tab.classList.contains("active")) {
+            const category = tab.id; // Assuming the tab's ID matches the category
+            fetchDataAndDisplay(category);
+        } else {
+            tabsBox.querySelector(".active").classList.remove("active");
+            tab.classList.add("active");
+        }
+    });
+});
 
 // Function for Scrolling Left and Right
 arrowIcons.forEach(icon => {
@@ -23,12 +78,6 @@ arrowIcons.forEach(icon => {
     });
 });
 
-allTabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tabsBox.querySelector(".active").classList.remove("active");
-        tab.classList.add("active");
-    });
-});
 
 // Function for Dragging
 const dragging = (e) => {
@@ -60,47 +109,6 @@ backToTopButton.innerHTML = '<a href="#top">Back to Top</a>';
 document.body.appendChild(backToTopButton);
 
 backToTopButton.addEventListener('click', scrollToTop);
-
-// Function to fetch data based on selected categories and update placesList
-function fetchData(selectedCategories) {
-    const apiUrl = `https://api.geoapify.com/v2/places?categories=${selectedCategories.join(',')}&filter=place:51482915cd16265e4059e735029a78663040f00101f90159cdd40000000000c0020992030642616775696f&limit=20&apiKey=${apiKey}`;
-    
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Clear the places list
-            placesList.innerHTML = '';
-
-            // Iterate through the fetched data and display it
-            data.features.forEach(place => {
-                const properties = place.properties;
-                const raw = properties.datasource.raw;
-
-                placesList.innerHTML += `
-                    <div class="place">
-                        <h3>${properties.name || 'N/A'}</h3>
-                        <p>Address: ${properties.formatted || 'N/A'}</p>
-                        <p>Email: ${raw['contact:email'] || 'N/A'}</p>
-                        <p>Phone: ${raw['contact:phone'] || 'N/A'}</p>
-                        <p>Mobile: ${raw['contact:mobile'] || 'N/A'}</p>
-                        <p>Website: ${raw['contact:website'] || 'N/A'}</p>
-                        <p>Facebook: ${raw['contact:facebook'] || 'N/A'}</p>
-                        <p>Opening Hours: ${raw.opening_hours || 'N/A'}</p>
-                        <p>Charge: ${raw.charge || 'N/A'}</p>
-                    </div>
-                `;
-            });
-
-            // Scroll to the first item
-            if (data.features.length > 0) {
-                scrollToItem(allTabs[0]);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
-
 
 // Show or hide the "Back to Top" button based on the scroll position
 window.addEventListener('scroll', () => {
